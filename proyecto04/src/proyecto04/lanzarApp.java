50fb7ef4afa0ac190e23f2569ad80421ac52bfef
Violeta Clar <violeta.clar@opendeusto.es>
2022-10-31 18:17:10 +0100

    Pruebas con main

--- proyecto04/src/proyecto04/lanzarApp.java
+++ proyecto04/src/proyecto04/lanzarApp.java
@@ -2,10 +2,9 @@ package proyecto04;
 
 import gestionBD.ConexionBD;
 
-public class main {
+public class lanzarApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		ConexionBD.realizarConexion();
 	}
 
