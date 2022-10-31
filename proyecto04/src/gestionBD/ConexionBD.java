a3939de01a95ded08a7ba778f07b95613a9bce4a
Violeta Clar <violeta.clar@opendeusto.es>
2022-10-31 18:08:46 +0100

    Realizada la conexion con BD

--- proyecto04/src/gestionBD/ConexionBD.java
+++ proyecto04/src/gestionBD/ConexionBD.java
@@ -2,6 +2,7 @@ package gestionBD;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConexionBD {
 
@@ -12,7 +13,12 @@ public class ConexionBD {
 			System.out.println("No se ha podido cargar el driver de la base de datos");
 		}
 		
-		try (Connection conn = DriverManager.getConnection("jdbc:sqlite:ficheros/ejemplo.db")) {
+		try (Connection con = DriverManager.getConnection("jdbc:sqlite:ficheros/ejemplo.db")) {
+			
+		} catch (SQLException e) {
+			// No se ha podido obtener la conexión a la base de datos
+			System.out.println("Error. No se ha podido conectar a la base de datos " + e.getMessage());
+		}
 	}
 
 }
