53ee452ec6a0408661ddf979671e02bb956921bb
Ane Pradera <ane.pradera@opendeusto.es>
2022-11-05 14:22:38 +0100

    Clase persona creada

---
+++ proyecto04/src/personas/Persona.java
@@ -0,0 +1,69 @@
+package personas;
+
+public class Persona {
+	
+	private int dni;
+	private String nombre;
+	private String apellido;
+	private String usuario;
+	private String contrasena;
+	
+	public Persona(int dni, String nombre, String apellido, String usuario, String contrasena) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.usuario = usuario;
+		this.contrasena = contrasena;
+	}
+
+	public int getDni() {
+		return dni;
+	}
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
