a00b6f084e75371298ac8d13f5905346690e161a
Ane Pradera <ane.pradera@opendeusto.es>
2022-11-05 14:28:36 +0100

    Clase administrador creada

---
+++ proyecto04/src/personas/Administrador.java
@@ -0,0 +1,13 @@
+package personas;
+
+public class Administrador extends Persona {
+
+	public Administrador(int dni, String nombre, String apellido, String usuario, String contrasena) {
+		super(dni, nombre, apellido, usuario, contrasena);
+	}
+	
+	
+	
+	
+
+}
