623b4ba3eae763f5d0a5be607a7a2b060b257577
Violeta Clar <violeta.clar@opendeusto.es>
2022-11-05 19:55:02 +0100

    Creada la clase para unittests

---
+++ proyecto04/src/ITests/gestionBD/conexionBD/IGestionPartidas.java
@@ -0,0 +1,14 @@
+package ITests.gestionBD.conexionBD;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IGestionPartidas {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
