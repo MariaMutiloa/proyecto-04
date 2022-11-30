410840bcb7a9f3bbe3137dd7a5604df8b784811f
Violeta Clar <violeta.clar@opendeusto.es>
2022-11-30 09:14:34 +0100

    Creadas clases test unitario

---
+++ proyecto04/testCase/testszonaAdministrador/partidaNueva/ConfiguracionPartTest.java
@@ -0,0 +1,14 @@
+package testszonaAdministrador.partidaNueva;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConfiguracionPartTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ proyecto04/testCase/testszonaAdministrador/partidaNueva/PartidaNuevaTest.java
@@ -0,0 +1,14 @@
+package testszonaAdministrador.partidaNueva;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PartidaNuevaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
