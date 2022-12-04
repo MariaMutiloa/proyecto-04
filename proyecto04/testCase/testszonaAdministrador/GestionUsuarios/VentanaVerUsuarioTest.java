accc750c044d02cf79f4f841d69cb45eaebf4e6f
Maria Mutiloa <maria.mutiloa@opendeusto.es>
2022-12-04 11:54:54 +0100

    Corrección JUnit anyadirUsuarios

--- proyecto04/testCase/testszonaAdministrador/GestionUsuarios/VentanaVerUsuarioTest.java
+++ proyecto04/testCase/testszonaAdministrador/GestionUsuarios/VentanaVerUsuarioTest.java
@@ -3,6 +3,7 @@ package testszonaAdministrador.GestionUsuarios;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -13,11 +14,22 @@ public class VentanaVerUsuarioTest {
 
 	@Test
 	public void testAnyadirUsuarios() {
-		Usuario u = new Usuario(35468792, "Pedro", "Rodriguez", "pedro.rodriguez", "rodri01", 1, 1000);
-		ArrayList<Usuario> listaUsuarios = new ArrayList<Usuario>();
-		listaUsuarios.add(u);
-		assertEquals(listaUsuarios, VentanaVerUsuario.anyadirUsuarios(listaUsuarios));
+		try {
+			List<Usuario> listaUsuarios = new ArrayList<Usuario>();
+			Usuario u1 = new Usuario(24356782, "Ruben", "Garcia", "ruben.garcia", "ruben01", 1, 10);
+			Usuario u2 = new Usuario(56478903, "Alba", "Rodriguez", "albarodri", "rodriguez17", 1, 10);
+			Usuario u3 = new Usuario(67456718, "Jon", "Lopez", "jonlopez", "lopezjon", 1, 10);
+			Usuario u4 = new Usuario(72554022, "Ane", "Pradera", "anepradera", "ane", 1, 10);
+			listaUsuarios.add(u1);
+			listaUsuarios.add(u2);
+			listaUsuarios.add(u3);
+			listaUsuarios.add(u4);
+
+
+			assertEquals(listaUsuarios, VentanaVerUsuario.anyadirUsuarios(listaUsuarios));
+		} catch (Exception e) {
+			
+		}
 
 	}
-
 }
