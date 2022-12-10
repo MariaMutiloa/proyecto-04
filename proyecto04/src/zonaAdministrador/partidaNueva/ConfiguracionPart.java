43148a3b0c9b92d1bd9f746cb414c4f8db213ad9
Violeta Clar <violeta.clar@opendeusto.es>
2022-12-10 20:48:01 +0100

    Tamaño ventana

--- proyecto04/src/zonaAdministrador/partidaNueva/ConfiguracionPart.java
+++ proyecto04/src/zonaAdministrador/partidaNueva/ConfiguracionPart.java
@@ -43,7 +43,7 @@ public class ConfiguracionPart extends JFrame {
 
 	public ConfiguracionPart(VentanaPrincipalAdmin parent, Administrador admin) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 400, 300);
+		setBounds(100, 100, 400, 350);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setVisible(true);
