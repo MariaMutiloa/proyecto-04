6c6bb62dc0193582b6f6c2b584646906e1950ec5
Violeta Clar <violeta.clar@opendeusto.es>
2023-01-08 17:43:05 +0100

    borrado metodo innecesario

--- proyecto04/src/gestionBD/GestionPartidas.java
+++ proyecto04/src/gestionBD/GestionPartidas.java
@@ -230,35 +230,6 @@ public class GestionPartidas {
 	}
 
 
-	public static Integer[] getLigas() {
-		Set<Integer> ligas = new TreeSet<Integer>();
-		logger.info("Extrayendo las ligas creadas");
-		try (Connection con = DriverManager.getConnection("jdbc:sqlite:DatosBingo.db")){
-			
-
-			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT * FROM LIGA");
-
-			
-			while(rs.next()) {
-				ligas.add(rs.getInt(1));
-			}
-			
-			logger.info("Ligas extraidas");
-		} catch (Exception e) {
-			e.printStackTrace();
-			JOptionPane.showMessageDialog(null,"No se han podido extraer las ligas", "Error", JOptionPane.ERROR_MESSAGE);
-			logger.log(Level.SEVERE, "No se ha podido acceder a la base de datos");
-		}
-		
-		Integer[] arrayLigas = new Integer[ligas.size()];
-		int contador = 0;
-		for(Integer i:ligas) {
-			arrayLigas[contador] = i;
-			contador++;
-		}
-		return arrayLigas;
-	}
 
 
 	public static void noEsBingo(Carton cartonGanador) {
