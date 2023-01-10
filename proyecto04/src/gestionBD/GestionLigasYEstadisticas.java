7e091826446c9894471eeef857cf1b429aae709d
AnaElduayen <elduayenana@gmail.com>
2023-01-10 15:05:54 +0100

    Creación clase GestionLigasYEstadisticas

---
+++ proyecto04/src/gestionBD/GestionLigasYEstadisticas.java
@@ -0,0 +1,7 @@
+package gestionBD;
+
+import java.util.logging.Logger;
+
+public class GestionLigasYEstadisticas {
+	private static Logger logger = Logger.getLogger(GestionPartidas.class.getName());
+}
