dffcf878520cf2728e3c93f4f56c354643c50dcb
AnaElduayen <elduayenana@gmail.com>
2023-01-10 15:12:11 +0100

    Creación de la tabla

--- proyecto04/src/zonaAdministrador/gestionLigas/TablaLigas.java
+++ proyecto04/src/zonaAdministrador/gestionLigas/TablaLigas.java
@@ -1,7 +1,36 @@
 package zonaAdministrador.gestionLigas;
 
+import java.util.List;
+
 import javax.swing.table.AbstractTableModel;
+
+import personas.Usuario;
 
 public class TablaLigas extends AbstractTableModel{
 
+
+	private String[] cabecera = { "Usuario", "Nombre", "Apelllido", "Bote" };
+	private List<Usuario> usuarios;
+
+	public TablaLigas(List<Usuario> lista) {
+		this.usuarios = lista;
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
