411672722524bd2966120fa1f92ce6d03ca9fe45
Ane Pradera <ane.pradera@opendeusto.es>
2023-01-11 01:08:51 +0100

    junits

--- proyecto04/testCase/testsgestionBD/GestionZonaUsuarioTest.java
+++ proyecto04/testCase/testsgestionBD/GestionZonaUsuarioTest.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import elementosOrganizacion.Carton;
+import elementosOrganizacion.Partida;
+import gestionBD.GestionPartidas;
+import gestionBD.GestionUsuarios;
 import gestionBD.GestionZonaUsuario;
 import personas.Administrador;
 import personas.Usuario;
@@ -28,10 +32,35 @@ public class GestionZonaUsuarioTest {
 	}
 	
 	@Test
-	
 	public void testComprobarUsuario() {
 		boolean resultadoMetodo = GestionZonaUsuario.comprobarUsuario("jon.fernandez", bdTest);
 		assertFalse(resultadoMetodo);
 	}
+	
+	@Test
+	public void testBuscarUsuarioPorID() {
+		Usuario u = new Usuario(1, "prueba", "prueba", "prueba", "prueba", 1, 10);
+		GestionZonaUsuario.insertarUsuario(u.getDni(), u.getNombre(), u.getApellido(), u.getUsuario(), u.getContrasena(), bdTest);
+		Usuario uSacado = GestionZonaUsuario.buscarUsuarioPorID(1, bdTest);
+		assertTrue(u.equals(uSacado));
+		GestionUsuarios.eliminar(1, bdTest);
+	}
+	
+	@Test
+	public void testBuscarPartidaActiva() {
+		GestionPartidas.nueva(bdTest);
+		Partida p = GestionZonaUsuario.buscarPartidaActiva(bdTest);
+		assertNotNull(p);
+		GestionPartidas.setGanadorBingo(0, p, 0, bdTest);
+	}
+	
+	@Test
+	public void testActualizarCartera() {
+		GestionZonaUsuario.actualizarCarteraBD(72554022, 12, bdTest);
+		Usuario u = GestionZonaUsuario.getUsuario("anepradera", "ane", bdTest);
+		assertEquals(12, u.getBote(), 0);
+	}
+	
+	
 
 }
